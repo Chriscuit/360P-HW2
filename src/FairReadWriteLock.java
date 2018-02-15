0eebaad0daf8abb0f3346a7c280e9fa1f57d9ae4
Christopher R. Gill <chrisraygill@utexas.edu>
2018-02-15 10:05:43 -0600

    fixed fixed #2

--- src/FairReadWriteLock.java
+++ src/FairReadWriteLock.java
@@ -4,14 +4,13 @@ public class FairReadWriteLock {
 
 	int readers = 0;
 	int writers = 0;
-	int writeRequests = 0;
 
 	ArrayList<Boolean> queue = new ArrayList<>();
 
     public synchronized void beginRead() throws InterruptedException {
 
         queue.add(true);
-        while(writers > 0 || writeRequests > 0 || queue.get(0) == false) {
+        while(writers > 0 || queue.get(0) == false) {
 			wait();
         }
         queue.remove(0);
